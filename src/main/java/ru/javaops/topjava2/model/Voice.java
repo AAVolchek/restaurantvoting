6c30ec709e6c81441c6912cd441eb44a4bed06ad
AAVolchek <71441342+AAVolchek@users.noreply.github.com>
2022-05-04 17:24:25 +0200

    add model

---
+++ src/main/java/ru/javaops/topjava2/model/Voice.java
@@ -0,0 +1,50 @@
+package ru.javaops.topjava2.model;
+
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "voice", uniqueConstraints = {@UniqueConstraint(columnNames = {"registered_date", "user_id"},
+       name = "voice_unique_date_user_idx")})
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
+public class Voice extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private User user;
+
+    @Column(name = "registered_date",nullable = false, columnDefinition = "date default now()")
+    @DateTimeFormat(iso=DateTimeFormat.ISO.DATE)
+    @NotNull
+    private LocalDate registeredDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private Restaurant restaurant;
+
+    public Voice(User user, LocalDate registeredDate, Restaurant restaurant) {
+        this.user = user;
+        this.registeredDate = registeredDate;
+        this.restaurant = restaurant;
+    }
+
+    public Voice(Integer id, User user, LocalDate registeredDate, Restaurant restaurant) {
+        super(id);
+        this.user = user;
+        this.registeredDate = registeredDate;
+        this.restaurant = restaurant;
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,38 @@ INSERT INTO USER_ROLES (role, user_id)
 VALUES ('USER', 1),
        ('ADMIN', 2),
        ('USER', 2);
+
+INSERT INTO restaurant (name)
+VALUES ('Coqfighter Finsbury Park'),
+       ('Piazza Italiana'),
+       ('ZIMA Russian Restaurant');
+
+INSERT INTO menu (name,  restaurant_id, registered_data)
+VALUES ('Menu Coqfighter Finsbury Park', 1, now()),
+       ('LUNCH SET MENU', 2, now() - interval 1 DAY);
+
+INSERT INTO dish (name, price, restaurant_id)
+VALUES ('Honey Ginger Buffalo', '10', 1),
+       ('Fries', '3', 1),
+       ('Raspberry Lemonade', '4', 1),
+       ('ZUPPA CREMOSA DI CAVOLFIORE ARROSTO', '8', 2),
+       ('LASAGNA VEGETARIANA', '6', 2),
+       ('TIRAMISU CLASSICO CON GELATO AL CAFFÈ', '2', 2),
+       ('Green tea', '1', 2),
+       ('Pelmeni Sibirskiye', '12', 3),
+       ('Borsh Krasnodarsky', '9', 3),
+       ('black tea', '2', 3);
+
+INSERT INTO menu_dishes(menu_id, dish_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (2, 4),
+       (2, 5),
+       (2, 6),
+       (2, 7);
+
+INSERT INTO voice(user_id, registered_date, restaurant_id)
+values (2, now(), 1)
+
+
